86b751318a461ada99030303afd35e18decfbcd6
Felix Ouma <fatonoh@gmail.com>
2023-01-13 16:18:03 +0300

    Add footer componet to the homepage

---
+++ src/components/Footer.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import footerText from '../data/footer';
+import logo from '../images/logo.png';
+import List from './List';
+
+const Footer = () => (
+  <footer className="flex flex-row items-center justify-between flex-wrap bg-black text-slate-400 p-4 py-6 my-14">
+    <div className="md:w-1/2 flex flex-col items-center md:items-start">
+      <img src={logo} width="137px" alt="Movies Logo" className="rounded py-2" />
+      <p className="mt-2 mr-6 text-sm">
+        {footerText.description}
+      </p>
+      <div className="text-white text-sm text-left py-4">
+        <a href="https://github.com/felix45" target="_blank" rel="noreferrer">
+          <FontAwesomeIcon icon={['fab', 'github']} size="2x" />
+          <span className="ml-2">{footerText.twitter}</span>
+        </a>
+      </div>
+      <div className="text-sm py-4 text-movie-gray"><em>{footerText.disclaimer}</em></div>
+    </div>
+    <div className="md:w-1/2">
+      <h3 className="text-white">Links</h3>
+      <div className="flex flex-row text-sm text-movie-gray">
+
+        {
+        footerText.links.map((link) => <List key={uuidv4()} items={link} />)
+      }
+      </div>
+    </div>
+  </footer>
+);
+
+export default Footer;
