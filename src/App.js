6872b9855e623c5a85a1987c95f9348980d96ec1
Felix Ouma <fatonoh@gmail.com>
2023-01-14 22:48:22 +0300

    Add user login/signup modal dialog

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+import React, { useState } from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import Modal from './modals/Modal';
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <main>
-      <Navbar />
+      <Navbar setIsOpen={setIsOpen} />
       <Footer />
+      { isOpen && <Modal setIsOpen={setIsOpen} /> }
     </main>
   );
 }
---
+++ src/modals/Modal.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  solid,
+} from '@fortawesome/fontawesome-svg-core/import.macro';
+import Register from './Register';
+import Login from './Login';
+
+const Modal = ({ setIsOpen }) => {
+  const [register, setRegister] = useState(true);
+
+  return (
+    <div
+      className="fixed inset-0 bg-slate-300 bg-opacity-95 overflow-y-auto h-full w-full"
+      id="my-modal"
+    >
+      <div
+        className="relative top-20 mx-auto p-5 border-0 w-96 shadow-lg rounded-md bg-movie-black"
+      >
+        <div>
+          <button
+            type="button"
+            className="absolute text-white -right-3 -top-5 bg-teal-500 p-3 rounded-full shadow-sm"
+            onClick={() => setIsOpen(false)}
+          >
+            <FontAwesomeIcon icon={solid('times')} size="2x" />
+          </button>
+        </div>
+        <div className="mt-3 text-left">
+          {
+            register ? <Register setRegister={setRegister} /> : <Login setRegister={setRegister} />
+          }
+        </div>
+      </div>
+    </div>
+  );
+};
+
+Modal.propTypes = {
+  setIsOpen: PropTypes.func.isRequired,
+};
+
+export default Modal;
