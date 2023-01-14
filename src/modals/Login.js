eb31016decca3e0e16f3f593ee60f06981669a3a
Felix Ouma <fatonoh@gmail.com>
2023-01-14 23:21:19 +0300

    Add user login event handler

--- src/modals/Login.js
+++ src/modals/Login.js
@@ -4,52 +4,52 @@ import PropTypes from 'prop-types';
 const Login = ({ setRegister }) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  
+
   const handleSubmit = (e) => {
     e.preventDefault();
   };
 
   return (
-  <div className="items-center px-4 py-3">
-    <h3 className="text-lg leading-6 font-medium text-white mb-5">Sign In</h3>
-    <form onSubmit={handleSubmit}>
-      <div>
-        <input
-          type="email"
-          className="form-input bg-white pr-4 pl-2 py-2 w-full rounded-md outline-none mb-5"
-          placeholder="Enter email..."
-          autoComplete="off"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-      </div>
-      <div>
-        <input
-          type="password"
-          className="form-input bg-white pr-4 pl-2 py-2 w-full rounded-md outline-none mb-5"
-          placeholder="Enter password..."
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          autoComplete="off"
-        />
-      </div>
-      <button
-        id="ok-btn"
-        type="submit"
-        className="px-4 py-2 bg-teal-500 text-white text-base font-medium rounded-md w-full shadow-sm hover:bg-teal-500"
-      >
-        Login
-      </button>
-    </form>
-    <div className="mt-0 px-0 py-3">
-      <p className="text-sm text-left text-movie-gray">
-        Dont have an account?
-        <button type="button" onClick={() => setRegister(true)} className="text-teal-500 ml-2">
-          Sign Up
+    <div className="items-center px-4 py-3">
+      <h3 className="text-lg leading-6 font-medium text-white mb-5">Sign In</h3>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <input
+            type="email"
+            className="form-input bg-white pr-4 pl-2 py-2 w-full rounded-md outline-none mb-5"
+            placeholder="Enter email..."
+            autoComplete="off"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div>
+          <input
+            type="password"
+            className="form-input bg-white pr-4 pl-2 py-2 w-full rounded-md outline-none mb-5"
+            placeholder="Enter password..."
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            autoComplete="off"
+          />
+        </div>
+        <button
+          id="ok-btn"
+          type="submit"
+          className="px-4 py-2 bg-teal-500 text-white text-base font-medium rounded-md w-full shadow-sm hover:bg-teal-500"
+        >
+          Login
         </button>
-      </p>
+      </form>
+      <div className="mt-0 px-0 py-3">
+        <p className="text-sm text-left text-movie-gray">
+          Dont have an account?
+          <button type="button" onClick={() => setRegister(true)} className="text-teal-500 ml-2">
+            Sign Up
+          </button>
+        </p>
+      </div>
     </div>
-  </div>
   );
 };
 
