a1d4844a466ab7289b51e94c4ca4b3092009c009
Felix Ouma <fatonoh@gmail.com>
2023-01-15 20:44:23 +0300

    Pass redux store to react application

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React, { useState } from 'react';
+import { Provider } from 'react-redux';
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
 import './App.css';
+import store from './redux/store';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Modal from './modals/Modal';
 
-function App() {
+const persistor = persistStore(store);
+const App = () => {
   const [isOpen, setIsOpen] = useState(false);
   return (
-    <main>
-      <Navbar setIsOpen={setIsOpen} />
-      <Footer />
-      { isOpen && <Modal setIsOpen={setIsOpen} /> }
-    </main>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <main>
+          <Navbar setIsOpen={setIsOpen} />
+          <Footer />
+          { isOpen && <Modal setIsOpen={setIsOpen} /> }
+        </main>
+      </PersistGate>
+    </Provider>
   );
-}
+};
 
 export default App;
