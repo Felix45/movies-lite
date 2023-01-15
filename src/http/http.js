10c00ed2e1edbc3dbdd4ff3f8a579b1063d0bdcd
Felix Ouma <fatonoh@gmail.com>
2023-01-15 20:42:35 +0300

    Add axios http instance

--- src/http/http.js
+++ src/http/http.js
@@ -1,5 +1,5 @@
-import axios from "axios";
+import axios from 'axios';
 
-const http = axios.create({baseURL: 'https://rails-kyhn.onrender.com/'});
+const http = axios.create({ baseURL: 'https://rails-kyhn.onrender.com/' });
 
 export default http;
