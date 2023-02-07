31fa9302597cef8b76effa86cdd3fdcc5c7ff0f5
Felix Ouma <fatonoh@gmail.com>
2023-02-07 03:26:44 +0300

    Add share page section to the homepage

---
+++ src/data/share.js
@@ -0,0 +1,3 @@
+const shareText = 'Just a better place to watch movies online for free.It allows you to watch movies online in high quality for free. No registration is required. The content is updated daily with fast streaming servers, multi-language subtitles supported. Just open and watch your favorite movies, tv-shows. We have almost any movie, tv-shows you want to watch!';
+
+export default shareText;
