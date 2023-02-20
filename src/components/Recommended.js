efac8e5ecdca6a196eaa9bd575d7c18fc7ebe4c4
Felix Ouma <fatonoh@gmail.com>
2023-02-21 00:08:54 +0300

    Fix margin errors on mobile

--- src/components/Recommended.js
+++ src/components/Recommended.js
@@ -7,21 +7,21 @@ import { useSelector } from 'react-redux';
 import MovieList from './movies/MoviesList';
 
 const RecommendedShows = () => {
-  const [category, setCategory] = useState('movie');
+  const [category, setCategory] = useState({ cat: 'movie', tab: 'mv' });
   const { featured } = useSelector((state) => state.featured);
 
-  const shows = featured.results.filter((show) => show.media_type === category);
+  const shows = featured.results.filter((show) => show.media_type === category.cat);
 
   return (
-    <section className="container mt-20 mx-auto text-slate-400">
+    <section className="container mt-5 md:mt-10 mx-auto text-slate-400">
       <div className="flex flex-col md:flex-row">
         <h2 className="ml-4 md:ml-0 font-extralight text-3xl">Recommended</h2>
         <div className="m-3 md:m-0">
-          <button type="button" onClick={() => { setCategory('movie'); }} className="rounded text-xs text-white bg-movie-green p-2 ml-2">
+          <button type="button" onClick={() => { setCategory({ cat: 'movie', tab: 'mv' }); }} className="rounded text-xs text-white bg-movie-green p-2 ml-2">
             <FontAwesomeIcon icon={solid('play-circle')} />
             Movies
           </button>
-          <button type="button" onClick={() => { setCategory('tv'); }} className="rounded text-xs bg-movie-black hover:text-white hover:bg-movie-green p-2 ml-2">
+          <button type="button" onClick={() => { setCategory({ cat: 'tv', tab: 'tv' }); }} className="rounded text-xs bg-movie-black hover:text-white hover:bg-movie-green p-2 ml-2">
             <FontAwesomeIcon icon={solid('list-ul')} />
             TV Shows
           </button>
