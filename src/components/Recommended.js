86de042557ca88fdbf95fd24b78dce290726c7f3
Felix Ouma <fatonoh@gmail.com>
2023-02-20 23:39:53 +0300

    Filter Tv shows based on category

--- src/components/Recommended.js
+++ src/components/Recommended.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   solid,
@@ -7,27 +7,27 @@ import { useSelector } from 'react-redux';
 import MovieList from './movies/MoviesList';
 
 const RecommendedShows = () => {
+  const [category, setCategory] = useState('movie');
   const { featured } = useSelector((state) => state.featured);
+
+  const shows = featured.results.filter((show) => show.media_type === category);
+
   return (
     <section className="container mt-20 mx-auto text-slate-400">
       <div className="flex flex-col md:flex-row">
         <h2 className="ml-4 md:ml-0 font-extralight text-3xl">Recommended</h2>
         <div className="m-3 md:m-0">
-          <button type="button" className="rounded text-xs text-white bg-movie-green p-2 ml-2">
+          <button type="button" onClick={() => { setCategory('movie'); }} className="rounded text-xs text-white bg-movie-green p-2 ml-2">
             <FontAwesomeIcon icon={solid('play-circle')} />
             Movies
           </button>
-          <button type="button" className="rounded text-xs bg-movie-black hover:text-white hover:bg-movie-green p-2 ml-2">
+          <button type="button" onClick={() => { setCategory('tv'); }} className="rounded text-xs bg-movie-black hover:text-white hover:bg-movie-green p-2 ml-2">
             <FontAwesomeIcon icon={solid('list-ul')} />
             TV Shows
           </button>
-          <button type="button" className="rounded text-xs bg-movie-black hover:text-white hover:bg-movie-green p-2 ml-2">
-            <FontAwesomeIcon icon={solid('line-chart')} />
-            Trending
-          </button>
         </div>
       </div>
-      <MovieList shows={featured.results} />
+      <MovieList shows={shows} />
     </section>
   );
 };
