0630e33b3179e935e927e54aeed9f67a9de7d499
Felix Ouma <fatonoh@gmail.com>
2023-02-20 16:15:51 +0300

    Make recommended section responsive on mobile

--- src/components/Recommended.js
+++ src/components/Recommended.js
@@ -5,9 +5,9 @@ import {
 } from '@fortawesome/fontawesome-svg-core/import.macro';
 
 const RecommendedShows = () => (
-  <section className="flex container mt-20 mx-auto text-slate-400">
-    <h2 className="font-extralight text-3xl">Recommended</h2>
-    <div className="ml-3">
+  <section className="flex flex-col md:flex-row container mt-20 mx-auto text-slate-400">
+    <h2 className="ml-4 md:ml-0 font-extralight text-3xl">Recommended</h2>
+    <div className="m-3 md:m-0">
       <button type="button" className="rounded text-xs text-white bg-movie-green p-2 ml-2">
         <FontAwesomeIcon icon={solid('play-circle')} />
         {' '}
