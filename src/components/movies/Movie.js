103022467cac8fae7416ce83421e82000edc4059
Felix Ouma <fatonoh@gmail.com>
2023-02-27 03:26:12 +0300

    Add link to movie details page on individual movie items

--- src/components/movies/Movie.js
+++ src/components/movies/Movie.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { NavLink } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   solid,
@@ -12,12 +13,16 @@ const Movie = ({ show, category }) => {
   return (
     <li className="mb-5">
       <div className="relative play-button">
-        <img src={`${IMG_URL}${show.poster_path}`} alt="" className="rounded" />
-        <div className="hidden flex absolute bg-movie-black justify-center opacity-70 items-center left-0 right-0 top-0 bottom-0">
-          <span className="flex items-center justify-center rounded-full bg-movie-green opacity-100 z-50 h-10 w-10 text-center">
-            <FontAwesomeIcon icon={solid('play')} className="text-white" />
-          </span>
-        </div>
+        <NavLink to={`/watch/${show.media_type || category}/${show.id}`}>
+          <img src={`${IMG_URL}${show.poster_path}`} alt="" className="rounded" />
+        </NavLink>
+        <NavLink to={`/watch/${show.media_type || category}/${show.id}`}>
+          <div className="hidden flex absolute bg-movie-black justify-center opacity-70 items-center left-0 right-0 top-0 bottom-0">
+            <span className="flex items-center justify-center rounded-full bg-movie-green opacity-100 z-50 h-10 w-10 text-center">
+              <FontAwesomeIcon icon={solid('play')} className="text-white" />
+            </span>
+          </div>
+        </NavLink>
       </div>
       <div className="text-white text-xs h-10 mt-3">{show.title || show.original_title || show.name }</div>
       <div className="flex flex-row items-center justify-between">
