6d042065448ee031df21cabbe5def30beaef94ad
felix45 <fatonoh@gmail.com>
2023-04-30 00:59:09 +0300

    Add responsive UI to list movies component

--- src/components/movies/MoviesList.js
+++ src/components/movies/MoviesList.js
@@ -4,7 +4,7 @@ import { v4 as uuidv4 } from 'uuid';
 import Movie from './Movie';
 
 const MovieList = ({ shows, category, cols }) => (
-  <ul className={`grid mt-8 grid-cols-2 gap-4 mx-4 sm:grid-cols-5 md:grid-cols-${cols} md:mx-0`}>
+  <ul className={`grid mt-8 grid-cols-2 gap-4 mx-4 md:grid-cols-${cols} md:mx-4`}>
     {
       shows.map((show) => show.poster_path
       && <Movie key={uuidv4()} show={show} category={category} />)
