d8051a5e7ee9211959483362f4d80fea064788da
felix45 <fatonoh@gmail.com>
2023-05-28 18:19:05 +0300

    Add react-tailwind-select component

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@
 module.exports = {
   content: [
     './src/**/*.{js,jsx,ts,tsx}', './node_modules/tw-elements/dist/js/**/*.js',
+    './node_modules/react-tailwindcss-select/dist/index.esm.js',
   ],
   theme: {
     extend: {
