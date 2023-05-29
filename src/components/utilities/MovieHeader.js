11ed3cf3da9bdc15c61023d5a512866ff9c541cf
felix45 <fatonoh@gmail.com>
2023-05-29 18:01:49 +0300

    Refactor espisode listing component

--- src/components/utilities/MovieHeader.js
+++ src/components/utilities/MovieHeader.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-  solid,
-} from '@fortawesome/fontawesome-svg-core/import.macro';
+import PlayButton from './PlayButton';
 
 const MovieHeader = () => (
   <div className="flex justify-center mx-3 text-white bg-black py-4 rounded-md md:mx-0">
     <h2>
-      <FontAwesomeIcon icon={solid('play')} className="text-movie-green mr-2" />
+      <PlayButton />
       {' '}
       <strong>Movie Files</strong>
     </h2>
--- src/components/utilities/SeriesHeader.js
+++ src/components/utilities/SeriesHeader.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import { PropTypes } from 'prop-types';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-  solid,
-} from '@fortawesome/fontawesome-svg-core/import.macro';
 import { useSelector } from 'react-redux';
 import SeriesDropDown from './SeriesDropDown';
 import EpisodePallete from './EpisodePallete';
+import PlayButton from './PlayButton';
 
 const SeriesHeader = ({ seasons, url }) => {
   const { watch } = useSelector((state) => state.watch);
@@ -15,9 +12,7 @@ const SeriesHeader = ({ seasons, url }) => {
   return (
     <div className="flex flex-col mx-3 bg-movie-green text-white bg-black py-4 rounded-md md:mx-0">
       <div className="flex flex-row px-4">
-        <div className="flex flex-col justify-center items-center bg-movie-green p-1 rounded mr-2">
-          <FontAwesomeIcon icon={solid('play')} size="xs" className="p-1 text-white" />
-        </div>
+        <PlayButton />
         <SeriesDropDown seasons={seasons} url={url} />
       </div>
       <div>
