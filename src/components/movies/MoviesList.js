14895c45ffa3352aa87dbd1a79540a35c773fc8b
felix45 <fatonoh@gmail.com>
2023-08-25 13:12:18 +0300

    Divide show and movie list into columns

--- src/components/movies/MoviesList.js
+++ src/components/movies/MoviesList.js
@@ -3,8 +3,8 @@ import PropTypes from 'prop-types';
 import { v4 as uuidv4 } from 'uuid';
 import Movie from './Movie';
 
-const MovieList = ({ shows, category, cols }) => {
-  const rowsetting = `grid mt-8 grid-cols-2 gap-4 mx-4 md:grid-cols-${cols} md:mx-4`;
+const MovieList = ({ shows, category }) => {
+  const rowsetting = 'grid mt-8 grid-cols-2 gap-4 mx-4 md:grid-cols-5 md:mx-4 xl:grid-cols-10';
 
   return (
     <div>
@@ -19,7 +19,6 @@ const MovieList = ({ shows, category, cols }) => {
 };
 
 MovieList.propTypes = {
-  cols: PropTypes.string.isRequired,
   shows: PropTypes.instanceOf(Array).isRequired,
   category: PropTypes.string.isRequired,
 };
