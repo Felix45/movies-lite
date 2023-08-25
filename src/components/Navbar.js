4a28c2d1aa59ed536ae5eea01410ab3c7da9127b
felix45 <fatonoh@gmail.com>
2023-08-25 13:18:11 +0300

    Make navigation bar responsive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -18,7 +18,7 @@ const Navbar = ({ setIsOpen }) => {
   };
 
   return (
-    <nav className="flex items-center justify-between flex-wrap bg-black p-4">
+    <nav className="flex items-center justify-between bg-black p-4">
       <div className="flex items-center flex-shrink-0 order-2 text-white md:mr-6">
         <NavLink to="/">
           <img width="137" src={logo} alt="Movies Logo" />
@@ -29,16 +29,13 @@ const Navbar = ({ setIsOpen }) => {
           <FontAwesomeIcon icon={solid('list-ul')} size="2x" />
         </button>
       </div>
-      <div className="md:w-full md:block md:flex-grow lg:flex lg:items-center lg:w-auto order-3">
-        <div className="hidden md:block text-sm lg:flex-grow">
+      <div className="lg:w-full lg:block lg:flex-grow lg:flex lg:items-center lg:w-auto order-3">
+        <div className="hidden text-sm lg:block lg:flex-grow">
           <NavLink to="/" className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-8">
             Home
           </NavLink>
           <a href="/" className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-8">
             Genre
-          </a>
-          <a href="/" className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-8">
-            Country
           </a>
           <NavLink to="/movies" className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-8">
             Movies
@@ -51,7 +48,7 @@ const Navbar = ({ setIsOpen }) => {
           </a>
         </div>
         <div>
-          <form className="hidden md:flex bg-slate-300 flex-row-reverse relative rounded-full no-focus">
+          <form className="hidden lg:flex bg-slate-300 flex-row-reverse relative rounded-full no-focus">
             <input type="text" className="form-input bg-slate-300 pr-4 pl-2 py-2 outline-none rounded-full" placeholder="Enter your keywords..." autoComplete="off" />
             <button type="button" className="relative bg-slate-300 text-slate-400 px-4 pr-0 rounded-full">
               <FontAwesomeIcon icon={solid('search')} size="lg" />
@@ -59,22 +56,22 @@ const Navbar = ({ setIsOpen }) => {
           </form>
         </div>
         <div className="flex items-center">
-          <button type="button" className="md:hidden text-slate-400 px-4 pr-0 rounded-full">
+          <button type="button" className="lg:hidden text-slate-400 px-4 pr-0 rounded-full">
             <FontAwesomeIcon icon={solid('search')} size="2x" />
           </button>
-          <div className="flex md:items-center inline-block text-sm px-4 py-2 leading-none text-white border-white hover:border-transparent md:mt-4 lg:mt-0">
+          <div className="flex md:items-center inline-block text-sm px-4 py-0 leading-none text-white border-white hover:border-transparent lg:mt-0">
             <span className="hidden md:block mx-1">{user.username}</span>
             { !user.isLoggedIn ? (
-              <FontAwesomeIcon className="md:hidden mr-2" onClick={() => { setIsOpen(true); }} icon={solid('sign-in')} size="2x" />)
-              : (<FontAwesomeIcon className="md:hidden mr-2" onClick={handleLogout} icon={solid('user-circle')} size="3x" />)}
-            <FontAwesomeIcon className="hidden md:block" icon={solid('user-circle')} size="2x" />
+              <FontAwesomeIcon className="lg:hidden mr-2" onClick={() => { setIsOpen(true); }} icon={solid('sign-in')} size="2x" />)
+              : (<FontAwesomeIcon className="lg:hidden mr-2" onClick={handleLogout} icon={solid('user-circle')} size="3x" />)}
+            <FontAwesomeIcon className="hidden lg:block" icon={solid('user-circle')} size="2x" />
             {
               user.isLoggedIn ? (
                 <div className="flex">
-                  <button type="button" className="hidden md:block ml-2" onClick={handleLogout}> Logout </button>
+                  <button type="button" className="hidden xl:block ml-2" onClick={handleLogout}> Logout </button>
                 </div>
               )
-                : <button type="button" className="hidden md:block ml-2" onClick={() => { setIsOpen(true); }}> Login / Register</button>
+                : <button type="button" className="hidden xl:block ml-2" onClick={() => { setIsOpen(true); }}> Login / Register</button>
             }
           </div>
         </div>
