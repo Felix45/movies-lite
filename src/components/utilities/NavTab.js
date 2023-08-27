cfff7271809b5dec781575a165fbbe8dd4e779ca
felix45 <fatonoh@gmail.com>
2023-08-27 23:46:58 +0300

    Add trending shows component

---
+++ src/components/utilities/NavTab.js
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { trendingShowsThunk } from '../../redux/slices/trendingSlice';
+
+const NavTab = () => {
+  const dispatch = useDispatch();
+  const [period, setPeriod] = useState('day');
+
+  useEffect(() => {
+    dispatch(trendingShowsThunk({ period }));
+  }, [period]);
+
+  return (
+    <ul className="flex flex-row text-movie-gray">
+      <li>
+        <button type="button" onClick={() => setPeriod('day')} className="p-[5px] text-xs border-movie-gray border-[1px] rounded-md ml-2">
+          Day
+        </button>
+      </li>
+      <li>
+        <button type="button" onClick={() => setPeriod('week')} className="p-[5px] text-xs border-movie-gray border-[1px] rounded-md ml-2">
+          Week
+        </button>
+      </li>
+    </ul>
+  );
+};
+
+export default NavTab;
--- src/components/utilities/SuggestedShowList.js
+++ src/components/utilities/SuggestedShowList.js
@@ -4,19 +4,19 @@ import { v4 as uuidv4 } from 'uuid';
 import { NavLink } from 'react-router-dom';
 import { IMG_URL } from '../../http/http';
 
-const SuggestedShowList = ({ shows }) => (
+const SuggestedShowList = ({ shows, rank }) => (
   <ul className="mt-5">
-    { shows.map((show) => (
+    { shows.map((show, index) => (
       show.poster_path && (
       <NavLink to={`/watch/${show.media_type}/${show.id}`} key={uuidv4()}>
-        <li className="flex flex-row mb-3 bg-movie-black rounded">
-
+        <li className="flex flex-row mb-3 relative trend-list text-movie-gray bg-movie-black rounded-l-lg hover:bg-movie-green !hover:text-movie-black">
+          { rank && <span className="rank absolute top-4 -left-3 text-movie-green font-bold w-[30px] py-1 h-[30px] border-2 border-movie-green text-sm text-center rounded-full bg-black">{ index + 1 }</span> }
           <span>
-            <img src={`${IMG_URL}${show.poster_path}`} alt="" className="object-scale-down h-16 rounded mr-2" />
+            <img src={`${IMG_URL}${show.poster_path}`} alt="" className="object-scale-down h-16 rounded-l-lg mr-2" />
           </span>
-          <div className="flex flex-col text-xs pt-3">
-            <span className="text-white">{`${show.media_type.slice(0, 1).toUpperCase()}${show.media_type.slice(1)} / ${new Date(show.first_air_date).getFullYear() || 2023}`}</span>
-            <span className="text-white">{show.title || show.original_title || show.name }</span>
+          <div className="flex flex-col text-sm pt-3 hover:text-movie-black">
+            <span className="text-xs">{`${show.media_type.slice(0, 1).toUpperCase()}${show.media_type.slice(1)} / ${new Date(show.first_air_date).getFullYear() || 2023}`}</span>
+            <span className="text-white font-bold">{show.title || show.original_title || show.name }</span>
           </div>
         </li>
       </NavLink>
@@ -26,6 +26,7 @@ const SuggestedShowList = ({ shows }) => (
 );
 
 SuggestedShowList.propTypes = {
+  rank: PropTypes.bool.isRequired,
   shows: PropTypes.instanceOf(Array).isRequired,
 };
 
