ec71289994683ba6ce57d7cd0784298d1185e874
felix45 <fatonoh@gmail.com>
2024-09-15 22:44:01 +0300

    Update container widths on homepage

--- src/components/RecentShows.js
+++ src/components/RecentShows.js
@@ -14,7 +14,7 @@ const RecentlyUpdated = () => {
 
   return (
     <aside className="flex flex-col mt-10 mx-5 lg:ml-0">
-      <div className="container-fluid flex flex-row justify-between xl:container">
+      <div className="container flex flex-row justify-between">
         <div className="flex flex-row">
           <PlayButton />
           <h2 className="ml-0 md:ml-0 font-bold text-white text-3xl lg:text-2xl xl:text-3xl">Recently Updated</h2>
